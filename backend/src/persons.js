a5ae3c3616d05acfe96454740bdad0256995b656
luccachagas <luccachagas@eaportal.org>
2025-10-14 21:34:09 -0300

    salvando2

---
+++ backend/src/persons.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        name: "Guilherme Carvalho",
+        nickname: "nextage bb",
+        age: 16,
+        email: "guilhermecarvalho@gmail.com"
+    },
+    {
+        name: "Victor Fustte",
+        nickname: "vg",
+        age: 12,
+        email: "victorfustte@gmail.com"
+    },
+    {
+        name: "sophia leal",
+        nickname: "leal",
+        age: 20,
+        email: "sophialeal@gmail.com"
+    },
+    {
+        name: "laura rodrigues",
+        nickname: "laura ama vitor",
+        age: 17,
+        email: "laurinha@gmail.com"
+    },
+]
+
+export { persons } 

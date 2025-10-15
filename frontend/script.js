4d8f02d044cb3b55ac1b3c8795b07ba16065e5fa
luccachagas <luccachagas@eaportal.org>
2025-10-14 21:33:13 -0300

    salvando

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('button');
+const main = document.querySelector('main');
+button.addEventListener('click', )
+
+async function buscarInformacaoNoBackend() {
+    const persons = await fetch('http://localhost:3333').then((response) => response.json())
+    persons.map((person) => {
+       main.innerHTML += `
+          <section>
+            <h2>Nome: ${person.name} </h2>
+            <h3>Apelido: ${person.nickname}</h3>
+            <h3>Idade:  ${person.age} anos</h3>
+            <h3>E-mail: ${person.email}</h3>
+        </section>
+       `
+    })
+}
